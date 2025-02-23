bad8a97973080f252d761cc0629dbb714abdc224
sanika <sanikagavhane512@gmail.com>
2025-02-23 14:13:51 +0530

    Table

---
+++ TableApp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class TableApp
+{
+	public static void main(String x[])
+	{
+		Scanner obj=new Scanner(System.in);
+		int no=5;
+		for(int i=1;i<=10;i++)
+		{
+			n=no*i;
+			System.out.println(n);
+		}
+	}
+}
